8b8d79b55421c2648118396530c1ab5f97b8b59c
Тима Суеубаев <556sueubaev@mail.ru>
2021-10-20 02:23:05 +0300

    sd

--- read.js
+++ read.js
@@ -1,6 +1,7 @@
-import { getFirestore, getDocs, collection } from 'https://cdnjs.cloudflare.com/ajax/libs/firebase/9.1.2/firebase-firestore.min.js';
+import { getFirestore, getDocs, collection, initializeApp } from 'https://cdnjs.cloudflare.com/ajax/libs/firebase/9.1.2/firebase-firestore.min.js';
 
-const firebaseApp = firebase.initializeApp({
+
+const firebaseConfig = {
     apiKey: "AIzaSyCuU3_U0yblB7IHdY8CTJb3nY5SSdIlmK8",
     authDomain: "basr-d0079.firebaseapp.com",
     databaseURL: "https://basr-d0079-default-rtdb.firebaseio.com",
@@ -9,8 +10,10 @@ const firebaseApp = firebase.initializeApp({
     messagingSenderId: "250883445973",
     appId: "1:250883445973:web:c200dd55f9dfd739e0fe5c",
     measurementId: "G-3M0ZDER1R6"
-});
-const db = firebaseApp.firestore();
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 async function getTodos() {
     try {
         const todoRef = collection(db, 'Выходы');
