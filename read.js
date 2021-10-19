7a82eefdb04db93cb4cc89037ee30a60bfc07985
Тима Суеубаев <556sueubaev@mail.ru>
2021-10-20 02:26:07 +0300

    sd

--- read.js
+++ read.js
@@ -1,5 +1,5 @@
-import { getFirestore, getDocs, collection, initializeApp } from 'https://cdnjs.cloudflare.com/ajax/libs/firebase/9.1.2/firebase-firestore.min.js';
-
+import { getFirestore, getDocs, collection } from 'https://cdnjs.cloudflare.com/ajax/libs/firebase/9.1.2/firebase-firestore.min.js';
+import initializeApp from 'https://cdnjs.cloudflare.com/ajax/libs/firebase/9.1.2/firebase-app.min.js';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCuU3_U0yblB7IHdY8CTJb3nY5SSdIlmK8",
