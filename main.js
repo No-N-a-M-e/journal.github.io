f2af5354e13e3561cfa3c76f63c1ab83f68f6932
Тима Суеубаев <556sueubaev@mail.ru>
2021-10-20 00:01:46 +0300

    dsf

--- main.js
+++ main.js
@@ -28,6 +28,17 @@ function add() {
 };
 window.addEventListener("onclick", add());
 async function read() {
+    const firebaseApp = firebase.initializeApp({
+        apiKey: "AIzaSyCuU3_U0yblB7IHdY8CTJb3nY5SSdIlmK8",
+        authDomain: "basr-d0079.firebaseapp.com",
+        databaseURL: "https://basr-d0079-default-rtdb.firebaseio.com",
+        projectId: "basr-d0079",
+        storageBucket: "basr-d0079.appspot.com",
+        messagingSenderId: "250883445973",
+        appId: "1:250883445973:web:c200dd55f9dfd739e0fe5c",
+        measurementId: "G-3M0ZDER1R6"
+    });
+    const db = firebaseApp.firestore();
     const querySnapshot = await getDocs(collection(db, "Выходы"));
     querySnapshot.forEach((doc) => {
         console.log(`${doc.id} => ${doc.data()}`);
