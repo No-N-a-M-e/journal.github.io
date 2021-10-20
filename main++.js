8b75798ebc7f1ba9168e853a9b968537597d12e1
Тима Суеубаев <556sueubaev@mail.ru>
2021-10-20 11:17:51 +0300

    df

--- main++.js
+++ main++.js
@@ -1,3 +1,5 @@
+import { doc, getDocs } from "https://cdnjs.cloudflare.com/ajax/libs/firebase/9.1.2/firebase-firestore-lite.min.js";
+
 async function getCities(db, user) {
     const citiesCol = doc(db, 'Выходы', user);
     const citySnapshot = await getDocs(citiesCol);
