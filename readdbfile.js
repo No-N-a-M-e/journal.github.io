ce9c7124844462c4182ed7ed8b86a50dac72e7be
Тима Суеубаев <556sueubaev@mail.ru>
2021-10-21 15:40:05 +0300

    dg

--- readdbfile.js
+++ readdbfile.js
@@ -1,6 +1,5 @@
 import { collection, getDocs } from "https://www.gstatic.com/firebasejs/9.1.2/firebase-firestore-compat.js"
 
-
 async function readdb() {
     const querySnapshot = await getDocs(collection(db, "users"));
     // querySnapshot.forEach((doc) => {
