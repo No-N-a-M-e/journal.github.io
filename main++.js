1aadeea30dc3a07381565fa59c307d60ef1e800a
Тима Суеубаев <556sueubaev@mail.ru>
2021-10-21 14:17:06 +0300

    sd

--- main++.js
+++ main++.js
@@ -1,3 +1,17 @@
+function adddb() {
+    var user = window.localStorage.getItem('emailForSignIn');
+    var kuda = document.getElementById("flex").value;
+    var data_uxoda = document.getElementById("start").value;
+    var vrema_uxoda = document.getElementById("time").value;
+    var data_prixoda = document.getElementById("end").value;
+    var vrema_prixoda = document.getElementById("time_end").value;
+    db.collection('Выходы').doc(user).update({ kuda });
+    db.collection('Выходы').doc(user).update({ data_uxoda });
+    db.collection('Выходы').doc(user).update({ vrema_uxoda });
+    db.collection('Выходы').doc(user).update({ data_prixoda });
+    db.collection('Выходы').doc(user).update({ vrema_prixoda });
+}
+
 function readdb() {
     db.on('value', function(snapshot) {
         var uniqName = snapshot.name();
